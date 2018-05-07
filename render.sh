fab8d019a70a7af09eff864dbc530b22449b79cb
Sergii <sergii.savchenko@gmail.com>
2018-05-07 17:58:57 +0300

    Change rendering

--- render.sh
+++ render.sh
@@ -1,6 +1,5 @@
 rm doc.html || 0
 rm -rf ./assets || 0
 cp -r ./content/assets ./assets
-cat content/*.md > rendered.md
-markdown-cli-renderer rendered.md doc.html
-rm rendered.md
+cat content/*.md > README.md
+markdown-cli-renderer README.md README.html
